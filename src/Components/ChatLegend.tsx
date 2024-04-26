b2ff5383be3d92e2ba2d70ec51db4d4b91647dae
Reka <reka.reka.rn@gmail.com>
2024-04-26 18:07:32 +0700

    hotfix

--- src/Components/ChatLegend.tsx
+++ src/Components/ChatLegend.tsx
@@ -1,12 +1,12 @@
 import { Flex } from "@mantine/core";
-import { Box, Group, SimpleGrid, Text } from "@mantine/core";
+import { Box, SimpleGrid, Text } from "@mantine/core";
 
 const ChatLegend = () => {
   return (
     <SimpleGrid cols={3} className="chat-legend">
-      <Flex gap={4}><Box className="color-icon" bg="success4" mt={4}/><Text w={120}>Understanding situation</Text></Flex>
-      <Flex gap={4}><Box className="color-icon" bg="warning4" mt={4}/><Text w={120}>{`Recognizing who's involved`}</Text></Flex>
-      <Flex gap={4}><Box className="color-icon" bg="primary4" mt={4}/><Text w={120}>Giving advice</Text></Flex>
+      <Flex gap={4}><Box className="color-icon" bg="success.4" mt={4}/><Text w={120}>Understanding situation</Text></Flex>
+      <Flex gap={4}><Box className="color-icon" bg="warning.4" mt={4}/><Text w={120}>{`Recognizing who's involved`}</Text></Flex>
+      <Flex gap={4}><Box className="color-icon" bg="primary.4" mt={4}/><Text w={120}>Giving advice</Text></Flex>
     </SimpleGrid>
   )
 }
